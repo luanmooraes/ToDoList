3e47196043fa687fbb82dfa78dfdf956101377c8
unknown <luansilvamoraess@gmail.com>
2021-08-04 18:59:13 -0300

    useReducer - Gerenciamento de estados

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,27 +1,62 @@
-import React from 'react';
-import { FlatList, Text } from 'react-native';
+import React, {useReducer, useState} from 'react';
+import { FlatList } from 'react-native';
+import {sha256} from 'react-native-sha256';
 
 import { Container, InputContainer, Input, AddButton, AddButtonText, ListItem } from './styles';
 
 const Main = () => {
-    const data = [
-        {id:'1', title: 'Arroz', check: false},
-        {id:'2', title: 'Feijão', check: false},
-    ]
+    // O estado inicial recebe um array vazio
+    const inicialState = []
+    
+    // È criada uma funçao reducer que recebe como parametros padroes 
+    // o state e a action. A logica da funcao funciona da seguinte forma:
+    // Eu tenho um switch com uma expressao action.type com algumas instruçoes 
+    // para serem executadas a partir do case escolhido. No caso desse exemplo foi
+    // disparado o case ADD quando foi pressionado o botao de Adicionar. 
 
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'ADD':
+                return [...state, action.item]
+                
+            default:
+                return state;
+        }
+    }
+    
+    const [item, setItem] = useState('');
+    const [state, dispatch] = useReducer(reducer, inicialState);
+    
     return (
       <Container>
           <InputContainer>
             <Input 
                 placeholder="Adicionar Produto"
+                value={item}
+                onChangeText={
+                    text => {setItem(text)}
+                }
             />
-            <AddButton>
+            <AddButton 
+                onPress={async() => {
+                    const hashId = await sha256(item);
+                    dispatch({
+                        type: 'ADD',
+                        item: {
+                            id: hashId,
+                            title: item,
+                            check: false
+                        }
+                    })
+                    setItem('')
+                }}
+            >
                 <AddButtonText>+</AddButtonText>
             </AddButton>
 
           </InputContainer>
             <FlatList
-                data={data}
+                data={state}
                 renderItem={({item}) => 
                     <ListItem>{item.title}</ListItem>
                 }
